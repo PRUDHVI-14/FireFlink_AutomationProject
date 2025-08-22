1de17d11e3f5aa01ec2fd00a050f34bd61ee8c9c
Bandi Prudhvi Naraya <Bandi Prudhvi Naraya@FFE-Bandi-Prudhvi-Narayana>
2025-08-22 17:57:11 +0530

    tc_001dfghj branch is committed

--- src/test/java/practice/AdvanceSelenium/Tc_001defghjk.java
+++ src/test/java/practice/AdvanceSelenium/Tc_001defghjk.java
@@ -2,13 +2,20 @@ package practice.AdvanceSelenium;
 
 import org.testng.annotations.Test;
 
-public class Tc_001 {
+public class Tc_001defghjk {
 
 	@Test
 	public void sample() {
 		
 		
 		System.out.println("sample for pushing to local branch");
+		
+		
+		
+	}
+	
+	public void sample2() {
+		
 		
 		
 		
