4ea406e08372637dd59dfcaee05116b3217c52a0
Bandi Prudhvi Naraya <Bandi Prudhvi Naraya@FFE-Bandi-Prudhvi-Narayana>
2025-08-23 12:00:33 +0530

    sampleclass is committed

---
+++ src/main/java/ninjaCRMOnjectRepository/SampleClassForJenkins.java
@@ -0,0 +1,11 @@
+package ninjaCRMOnjectRepository;
+
+import org.testng.annotations.Test;
+
+public class SampleClassForJenkins {
+
+	@Test
+	public void sampleTest() {
+		System.out.println(" sample test executed for jenkins");
+	}
+}
