e0467d567fb6407dedffa0566554b61cfd95a548
Bandi Prudhvi Naraya <Bandi Prudhvi Naraya@FFE-Bandi-Prudhvi-Narayana>
2025-08-23 12:57:58 +0530

    readDataCMD committed

--- src/test/java/practice/AdvanceSelenium/ReadDatafromCmdLine.java
+++ src/test/java/practice/AdvanceSelenium/ReadDatafromCmdLine.java
@@ -11,7 +11,7 @@ public class ReadDatafromCmdLine {
 		System.out.println(BROWSER);
 		
 		String USERNAME = System.getProperty("username");
-		System.out.println(USERNAME);
+		System.out.println(USERNAME +" osnvcjasdb");
 		
 		String PASSWORD = System.getProperty("password");
 		System.out.println(PASSWORD);
